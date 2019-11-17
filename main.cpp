14f479881cd334b1e1a6a90d802cc00faa61e5b6
KeithFlame <38587125+KeithFlame@users.noreply.github.com>
2019-11-17 18:22:58 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,42 @@
 #include "member_shopping.h"
+#include"gtest/gtest.h"
 
-int main() {
+
+TEST(CMailTest, test_when_member_GOLD_buy_a_coffee) {
 	CMail oneshopping;
 	oneshopping.checkMemberRank('A');
-	oneshopping.countChocolates(5);
-	oneshopping.countCoffeeCups(0);
-	oneshopping.countFruitTea(0);
+	//oneshopping.countChocolates(0);
+	//oneshopping.countCoffeeCups(0);
+	//oneshopping.countFruitTea(0);
 	oneshopping.countNestCoffee(1);
-	oneshopping.calcCost();
-	return 0;
+	EXPECT_EQ(48, oneshopping.calcCost());
+}
+TEST(CMailTest, test_when_member_NON_buy_a_coffee) {
+	CMail oneshopping;
+	//oneshopping.checkMemberRank();
+	//oneshopping.countChocolates(0);
+	//oneshopping.countCoffeeCups(0);
+	//oneshopping.countFruitTea(0);
+	oneshopping.countNestCoffee(1);
+	EXPECT_EQ(48, oneshopping.calcCost());
+}
+
+TEST(CMailTest, test_when_member_NON_buy_a_coffee_and_5_chocolates) {
+	CMail oneshopping;
+	oneshopping.checkMemberRank('B');
+	oneshopping.countChocolates(5);
+	//oneshopping.countCoffeeCups(0);
+	//oneshopping.countFruitTea(0);
+	oneshopping.countNestCoffee(1);
+	EXPECT_EQ(279.8, oneshopping.calcCost());
+}
+
+TEST(CMailTest, test_when_member_NON_buy_a_coffee_and_1_chocolate_and_1_tea_and_3_cups) {
+	CMail oneshopping;
+	oneshopping.checkMemberRank('B');
+	oneshopping.countChocolates(5);
+	oneshopping.countCoffeeCups(3);
+	oneshopping.countFruitTea(1);
+	oneshopping.countNestCoffee(1);
+	EXPECT_EQ(562.9, oneshopping.calcCost());
 }
