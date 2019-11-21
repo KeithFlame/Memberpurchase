0e431bb4cb62083c5c2125520332a603a2dd19e4
jing.q.xu <jing.q.xu@hotmail.com>
2019-11-21 21:07:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "member_shopping.h"
 #include"gtest/gtest.h"
 
-
+//浮点数比较不要用EXPECT_EQ
+//测试用例的文件名最好采用test_XXXX.cpp
+//现实需求中，往往是商店里有上万种商品，而单个用户一次只会购买较少种类的商品。目前的实现会有啥问题？
+//考虑到实际的收银过程，同一种类的商品有可能会分几次扫描、计价，目前的实现能否满足需求？
+//例如：oneshopping.countNestCoffee(1); oneshopping.countNestCoffee(3); 最终结果应该给是48*3
 TEST(CMailTest, test_when_member_GOLD_buy_a_coffee) {
 	CMail oneshopping;
 	oneshopping.checkMemberRank('A');
